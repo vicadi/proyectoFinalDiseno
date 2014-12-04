5473810c52879bfe21ec233d8927d273b0038836
vicadi <vicadi7@gmail.com>
2014-12-04 11:40:59 -0500

    modificacion de requerimientos

--- public/js/script.js
+++ public/js/script.js
@@ -9,58 +9,5 @@ $(document).ready(function(){
 			else
 				$('#contenido #requerimientos table #reqPortafolio #imagenPlantilla').attr('src','/images/plantilla3.jpg');
 	});
-
-	var contador;
-	$('#todosPagos').click(function () {
-		if(contador==0){
-	       	$("#pago1").attr('checked', true);
-	       	$("#pago2").attr('checked', true);
-	       	$("#pago3").attr('checked', true);
-	       	$("#pago4").attr('checked', true);
-	       	$("#pago5").attr('checked', true);
-	       	$("#pago6").attr('checked', true);
-	       	$("#pago7").attr('checked', true);
-	       	$("#pago8").attr('checked', true);
-	       	$("#todosPagos").attr('checked', true);
-	       	contador=1;
-	    }else{
-	       	$("#pago1").attr('checked', false);
-	       	$("#pago2").attr('checked', false);
-	       	$("#pago3").attr('checked', false);
-	       	$("#pago4").attr('checked', false);
-	       	$("#pago5").attr('checked', false);
-	       	$("#pago6").attr('checked', false);
-	       	$("#pago7").attr('checked', false);
-	       	$("#pago8").attr('checked', false);
-	       	$("#todosPagos").attr('checked', false);
-	       	contador=0;
-	    }
-	});
-
-	$('#todosProm').click(function () {
-		if(contador==0){
-	       	$("#prom1").attr('checked', true);
-	       	$("#prom2").attr('checked', true);
-	       	$("#prom3").attr('checked', true);
-	       	$("#prom4").attr('checked', true);
-	       	$("#prom5").attr('checked', true);
-	       	$("#prom6").attr('checked', true);
-	       	$("#prom7").attr('checked', true);
-	       	$("#prom8").attr('checked', true);
-	       	$("#todosProm").attr('checked', true);
-	       	contador=1;
-	    }else{
-	       	$("#prom1").attr('checked', false);
-	       	$("#prom2").attr('checked', false);
-	       	$("#prom3").attr('checked', false);
-	       	$("#prom4").attr('checked', false);
-	       	$("#prom5").attr('checked', false);
-	       	$("#prom6").attr('checked', false);
-	       	$("#prom7").attr('checked', false);
-	       	$("#prom8").attr('checked', false);
-	       	$("#todosProm").attr('checked', false);
-	       	contador=0;
-	    }
-	});
 })
 
